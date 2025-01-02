8620aad77e0c6b45f939f80f2b28b35249e8a237
riyadhbd2 <riyadhbd2@gmail.com>
2025-01-02 21:16:09 +0100

    get and post api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 
@@ -30,6 +30,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db("carDoctor").collection("services");
+    const bookingCollection = client.db("carDoctor").collection("bookings");
+
+    // API to get all the data in client side
+    app.get('/services', async(req, res)=>{
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    // API to get a single document
+    app.get('/services/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)};
+        const result = await serviceCollection.findOne(query);
+        res.send(result);
+
+    })
+
+    // API to post a document
+    app.post('/bookings', async(req, res)=>{
+        const booking = req.body;
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
